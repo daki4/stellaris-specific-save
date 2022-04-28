1978a314dcde5fa977a98090bf90be4df29e87a0
daki4 <yordanmitev03@protonmail.com>
2022-04-28 03:44:46 +0300

    major new features

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use home;
 #[allow(unused)]
-use std::{env, fs, fs::read_dir, io, io::Read, path::Path};
-use std::{path::PathBuf, time::SystemTime};
+use log::{debug, error, info, warn};
+use std::{env, fs, fs::read_dir, path::Path, path::PathBuf, time::SystemTime};
 
-// get linux stellaris saves path
+/// get linux stellaris saves path
 #[cfg(target_os = "linux")]
 fn get_path() -> Option<String> {
     let home = home::home_dir()?;
@@ -14,7 +13,7 @@ fn get_path() -> Option<String> {
     Some(path)
 }
 
-// get windows stellaris saves path
+/// get windows stellaris saves path
 #[cfg(target_os = "windows")]
 fn get_path() -> Option<String> {
     let home = home::home_dir()?;
@@ -25,28 +24,24 @@ fn get_path() -> Option<String> {
     Some(path)
 }
 
-// get the location to store the saves
-fn get_target_path() -> String {
-    env::var("TARGET_DIR").unwrap()
-}
-
-// ensure the target directory exists
-fn ensure_target_dir(target_dir: &String) {
+/// ensure the target directory exists
+fn ensure_target_dir(target_dir: &str) {
     if !Path::new(target_dir).exists() {
         fs::create_dir_all(target_dir).unwrap();
     }
 }
 
-// copy stuff
+/// copy stuff
+#[allow(clippy::vec_init_then_push)]
 pub fn copy<U: AsRef<Path>, V: AsRef<Path>>(from: U, to: V) -> Result<(), std::io::Error> {
-    let mut stack = Vec::new();
+    let mut stack = vec![];
     stack.push(PathBuf::from(from.as_ref()));
 
     let output_root = PathBuf::from(to.as_ref());
     let input_root = PathBuf::from(from.as_ref()).components().count();
 
     while let Some(working_path) = stack.pop() {
-        println!("process: {:?}", &working_path);
+        debug!("process: {:?}", &working_path);
 
         let src: PathBuf = working_path.components().skip(input_root).collect();
 
@@ -56,7 +51,7 @@ pub fn copy<U: AsRef<Path>, V: AsRef<Path>>(from: U, to: V) -> Result<(), std::i
             output_root.join(&src)
         };
         if fs::metadata(&dest).is_err() {
-            println!(" mkdir: {:?}", dest);
+            debug!("mkdir: {:?}", dest);
             fs::create_dir_all(&dest)?;
         }
 
@@ -69,11 +64,11 @@ pub fn copy<U: AsRef<Path>, V: AsRef<Path>>(from: U, to: V) -> Result<(), std::i
                 match path.file_name() {
                     Some(filename) => {
                         let dest_path = dest.join(filename);
-                        println!("  copy: {:?} -> {:?}", &path, &dest_path);
+                        debug!("copy: {:?} -> {:?}", &path, &dest_path);
                         fs::copy(&path, &dest_path)?;
                     }
                     None => {
-                        println!("failed: {:?}", path);
+                        debug!("failed: {:?}", path);
                     }
                 }
             }
@@ -82,8 +77,8 @@ pub fn copy<U: AsRef<Path>, V: AsRef<Path>>(from: U, to: V) -> Result<(), std::i
     Ok(())
 }
 
-// function to get the last modified directory
-fn get_last_modified_directory(path: &String) -> String {
+/// function to get the last modified directory
+fn get_last_modified_directory(path: &str) -> String {
     let mut last_modified_time = SystemTime::UNIX_EPOCH;
     let mut last_modified_path = String::new();
     for entry in read_dir(&path).unwrap() {
@@ -97,30 +92,106 @@ fn get_last_modified_directory(path: &String) -> String {
     last_modified_path
 }
 
+pub fn get_last_modified_file(path: &str) -> String {
+    let mut last_modified_time = SystemTime::UNIX_EPOCH;
+    let mut last_modified_name = String::new();
+    for entry in fs::read_dir(path).unwrap() {
+        let entry = entry.unwrap();
+        if entry.metadata().unwrap().modified().unwrap() > last_modified_time {
+            last_modified_time = entry.metadata().unwrap().modified().unwrap();
+            // last_modified_name = name;
+            last_modified_name = Path::new(&entry.path())
+                .file_name()
+                .unwrap()
+                .to_os_string()
+                .into_string()
+                .unwrap();
+        }
+    }
+    last_modified_name
+}
+
+pub struct Configuration {
+    pub directory_saves: String,
+    pub directory_target: String,
+    pub delay_seconds: u64,
+    pub years_passed: u16,
+    pub default_delay_seconds: u64,
+    pub delimeter: String,
+}
+
+impl Configuration {
+    pub fn new() -> Configuration {
+        Configuration {
+            directory_saves: get_path().unwrap(),
+            directory_target: env::var("TARGET_DIR").unwrap(),
+            delay_seconds: env::var("DELAY_SECONDS").unwrap().parse().unwrap(),
+            years_passed: env::var("YEARS_PASSED").unwrap().parse().unwrap(),
+            default_delay_seconds: 5,
+            delimeter: match env::consts::OS {
+                "windows" => "\\".to_string(),
+                "linux" => "/".to_string(),
+                _ => panic!("unsupported OS"),
+            },
+        }
+    }
+}
+
+impl Default for Configuration {
+    fn default() -> Self {
+        Configuration::new()
+    }
+}
+
 fn main() {
+    env::set_var("RUST_LOG", "debug");
+    pretty_env_logger::init_timed();
     dotenv::dotenv().ok();
-    let directory_saves = get_path().unwrap();
-    let directory_target = get_target_path();
-    let target = get_target_path();
-    ensure_target_dir(&target);
-    let mut delimeter = "";
-    if cfg!(target_os = "linux") {
-        delimeter = "/";
-    }
-    else if cfg!(target_os = "windows") {
-        delimeter = "\\";
-    }
+
+    let conf = Configuration::new();
+    ensure_target_dir(&conf.directory_target);
+    let mut last_saved_year = 2200u16;
 
     loop {
-        let last_modified_directory = get_last_modified_directory(&directory_saves);
-        dbg!(&directory_saves, &directory_target, &last_modified_directory);
-        let temp = last_modified_directory.split(delimeter).collect::<Vec<&str>>();
+        let last_modified_directory = get_last_modified_directory(&conf.directory_saves);
 
-        let new_path = directory_target.clone() + delimeter.clone() + temp[temp.len() - 1].clone();
-        match copy(Path::new(&last_modified_directory), Path::new(&new_path)) {
-            Ok(()) => {}
-            Err(_e) => println!("failed to copy."),
+        if conf.years_passed > 0 {
+            let last_mod_file = get_last_modified_file(&last_modified_directory);
+            let save_year = last_mod_file.split(".").collect::<Vec<&str>>()[0]
+                .parse::<u16>()
+                .unwrap();
+            debug!(
+                "data: last_mod_file: '{}'; save_year: {}; last_saved_year: {}",
+                &last_mod_file, &save_year, &last_saved_year,
+            );
+            if save_year <= last_saved_year + &conf.years_passed - 1 {
+                std::thread::sleep(std::time::Duration::from_secs(*&conf.default_delay_seconds));
+                debug!("sleeping, no new-enough files found.");
+                continue;
+            }
+            last_saved_year = save_year;
         }
-        std::thread::sleep(std::time::Duration::from_secs(10));
+
+        let temp = last_modified_directory
+            .split(&conf.delimeter)
+            .collect::<Vec<&str>>();
+
+        let new_path = format!(
+            "{}{}{}",
+            &conf.directory_target,
+            &conf.delimeter,
+            temp[temp.len() - 1]
+        );
+        match copy(Path::new(&last_modified_directory), Path::new(&new_path)) {
+            Ok(()) => info!(
+                "backed up: {}",
+                get_last_modified_file(&last_modified_directory)
+            ),
+            Err(_e) => warn!("failed to copy: {}", &last_modified_directory),
+        }
+
+        if conf.delay_seconds > 0 && conf.years_passed == 0 {
+            std::thread::sleep(std::time::Duration::from_secs(conf.delay_seconds));
+        }
     }
 }
